5587c9661b96d42a886eb12cf286490bfba053b3
NahuelLu <nahuellucana1999@gmail.com>
2022-07-24 16:41:06 -0300

    Add button See library and connection with JS

--- scripts.js
+++ scripts.js
@@ -8,21 +8,52 @@ function Book(title,author,pages,read){
 //Definitions
 let myBooks=[];
 
-const inputName = document.querySelector("#name");
+const inputTitle = document.querySelector("#name");
 const inputAuthor = document.querySelector("#author");
 const inputPages = document.querySelector("#pages");
 const inputRead = document.querySelector("#read");
 const submitButton = document.querySelector("#form-button");
-
+const showBooksButton = document.querySelector("#display-books-btn");
+const divBooks = document.querySelector(".display-books-ctn")
 //Functions
 function addBookToLibrary(){ 
-    let name = inputName.value;
+    let title = inputTitle.value;
     let author = inputAuthor.value;
     let pages = inputPages.value;
     let readState = inputRead.value;
-    let newBook = new Book(name,author,pages,readState);
+    let newBook = new Book(title,author,pages,readState);
     myBooks.push(newBook);
 }
+function displayBooks(){
+    myBooks.forEach(book => {
+        createCard(book);
+    });
+}
+function createCard(book){
+    let card = document.createElement("div");
+    divBooks.appendChild(card);
+    card.className="card";
+    createDivsForBookInsideOf(card,book);
+}
+function createDivsForBookInsideOf(parent,book){
+    let titleDiv= document.createElement("div");
+    let authorDiv = document.createElement("div");
+    let pagesDiv= document.createElement("div");
+    let readDiv= document.createElement("div");
+    parent.appendChild(titleDiv);
+    parent.appendChild(authorDiv);
+    parent.appendChild(pagesDiv);
+    parent.appendChild(readDiv);
+    titleDiv.className="data"
+    authorDiv.className="data"
+    pagesDiv.className="data"
+    readDiv.className="data"
+    titleDiv.textContent=book.title;
+    authorDiv.textContent=book.author;
+    pagesDiv.textContent=book.pages;
+    readDiv.textContent=book.read;
+}
 //Event listeners
-submitButton.addEventListener("click",addBookToLibrary)
+submitButton.addEventListener("click",addBookToLibrary);
+showBooksButton.addEventListener("click",displayBooks);
 
